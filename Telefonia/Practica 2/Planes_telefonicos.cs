968e576aecf4230e2542d8014ec099e9cc432e87
Carlos <halomau@hotmail.com>
2018-08-21 22:13:09 -0500

    Añadí atributos

--- Telefonia/Practica 2/Planes_telefonicos.cs
+++ Telefonia/Practica 2/Planes_telefonicos.cs
@@ -4,6 +4,13 @@ namespace Practica_2
 {
     class Planes_telefonicos
     {
+        #region Atributos
+        ushort costo;
+        ushort megas;
+        byte llamadas;
+        byte mensajes;
+        #endregion
+
 
     }
 }
